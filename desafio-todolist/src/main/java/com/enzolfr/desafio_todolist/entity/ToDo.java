e63a56a2957c2830ae8cd48ad72494f7df03327e
EnzoLFranco <fenzolemos@gmail.com>
2025-01-13 14:43:12 -0300

    Feat: Adiciona os getters e setters da entidade ToDo

--- desafio-todolist/src/main/java/com/enzolfr/desafio_todolist/entity/ToDo.java
+++ desafio-todolist/src/main/java/com/enzolfr/desafio_todolist/entity/ToDo.java
@@ -13,5 +13,43 @@ public class ToDo {
     private boolean itsRealized;
     private int priority;
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isItsRealized() {
+        return itsRealized;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setItsRealized(boolean itsRealized) {
+        this.itsRealized = itsRealized;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
