63517d335ec4817dfd8f1f7666869cbb7784fd59
EnzoLFranco <fenzolemos@gmail.com>
2025-01-13 15:16:18 -0300

    Feat: Adiciona a classe TodoController

---
+++ desafio-todolist/src/main/java/com/enzolfr/desafio_todolist/controller/TodoController.java
@@ -0,0 +1,13 @@
+package com.enzolfr.desafio_todolist.controller;
+
+import com.enzolfr.desafio_todolist.entity.Todo;
+import com.enzolfr.desafio_todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+}
