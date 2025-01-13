11118cb940748ca42e0cb750289f0100896518f3
EnzoLFranco <fenzolemos@gmail.com>
2025-01-13 15:02:09 -0300

    Feat: Adiciona a classe TodoService

---
+++ desafio-todolist/src/main/java/com/enzolfr/desafio_todolist/service/TodoService.java
@@ -0,0 +1,13 @@
+package com.enzolfr.desafio_todolist.service;
+
+import com.enzolfr.desafio_todolist.entity.Todo;
+import com.enzolfr.desafio_todolist.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+}
