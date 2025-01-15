93a46ad2828f4f4f06142b3099475c7722241357
EnzoLFranco <fenzolemos@gmail.com>
2025-01-15 15:06:11 -0300

    Feat: Adiciona o teste referente ao success do create()

--- desafio-todolist/src/test/java/com/enzolfr/desafio_todolist/DesafioTodolistApplicationTests.java
+++ desafio-todolist/src/test/java/com/enzolfr/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -1,13 +1,33 @@
 package com.enzolfr.desafio_todolist;
 
+import com.enzolfr.desafio_todolist.entity.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DesafioTodolistApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreateTodoSucess() {
+		var todo = new Todo("todo 1","desc todo 1", false, 3);
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("$[0].name").isEqualTo(todo.getName())
+				.jsonPath("$[0].description").isEqualTo(todo.getDescription())
+				.jsonPath("$[0].itsRealized").isEqualTo(todo.isItsRealized())
+				.jsonPath("$[0].priority").isEqualTo(todo.getPriority());
 	}
+
 
 }
